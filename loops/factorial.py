e362ca9ab34a5bc5d08f80ab9f9d71426f71861d
Aman Kumar <amankumarmaurya93@gmail.com>
2024-09-24 22:51:29 +0530

    some changes

--- loops/factorial.py
+++ loops/factorial.py
@@ -1,9 +1,9 @@
 #Total Sum
-n = 7
-sum = 0
-for i in range(1, n+1):
-    sum += i
-    print("Total sum: ", sum)
+# n = 7
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+#     print("Total sum: ", sum)
 
 # factorial with while loop
 n = 4
